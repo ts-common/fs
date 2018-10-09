c6ca3045e5d0fc3abc620f3a10059dba464c87b9
Sergey Shandar <sergey.shandar@gmail.com>
2018-10-09 13:49:42 -0700

    0.0.0

---
+++ index.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs"
+
+export const readFile = (pathStr: string): Promise<Buffer> =>
+    new Promise(
+        (resolve, reject) => fs.readFile(
+            pathStr,
+            (err, data) => {
+                if (err) {
+                    reject(err)
+                }
+                resolve(data)
+            }
+        )
+    )
+
+export const exists = async (dir: string): Promise<boolean> =>
+    new Promise<boolean>(resolve => fs.exists(dir, resolve))
