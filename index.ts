5bbf0c9651ae22a6d1b1ee9e475160b8a9e5bbbb
Sergey Shandar <sergey.shandar@gmail.com>
2019-01-23 17:51:18 -0800

    Write File

--- index.ts
+++ index.ts
@@ -1,17 +1,35 @@
 import * as fs from "fs"
+import * as path from "path"
+
+type Callback<T> = (err: NodeJS.ErrnoException, result: T) => void
+
+const toPromise = <T>(f: (callback: Callback<T>) => void) =>
+    new Promise<T>((resolve, reject) =>f((err, result) => err ? reject(err) : resolve(result)))
+
+type CallbackVoid = (err: NodeJS.ErrnoException) => void
+
+const toPromiseVoid = (f: (callback: CallbackVoid) => void) =>
+    new Promise<void>((resolve, reject) =>f((err) => err ? reject(err) : resolve()))
 
 export const readFile = (pathStr: string): Promise<Buffer> =>
-    new Promise(
-        (resolve, reject) => fs.readFile(
-            pathStr,
-            (err, data) => {
-                if (err) {
-                    reject(err)
-                }
-                resolve(data)
-            }
-        )
-    )
+    toPromise(callback => fs.readFile(pathStr, callback))
+
+export const writeFile = (pathStr: string, data: string): Promise<void> =>
+    toPromiseVoid(callback => fs.writeFile(pathStr, data, callback))
+
+export const readdir = (pathStr: string): Promise<ReadonlyArray<fs.Dirent>> =>
+    toPromise(callback => fs.readdir(pathStr, { withFileTypes: true }, callback))
+
+export const recursiveReaddir = async function *(dir: string): AsyncIterableIterator<string> {
+    for (const f of await readdir(dir)) {
+        const p = path.join(dir, f.name)
+        if (f.isDirectory()) {
+            yield *recursiveReaddir(p)
+        } else {
+            yield path.join(dir, f.name)
+        }
+    }
+}
 
 export const exists = (dir: string): Promise<boolean> =>
-    new Promise<boolean>(resolve => fs.exists(dir, resolve))
+    new Promise(resolve => fs.exists(dir, resolve))
--- test/test.ts
+++ test/test.ts
@@ -15,6 +15,25 @@ describe("readFile", () => {
     })
 })
 
+describe("readdir", () => {
+    it("read current dir", async () => {
+        const result = await fsp.readdir("./")
+        const nm = result.filter(d => d.isDirectory && d.name === "node_modules")
+        assert.strictEqual(nm.length, 1)
+    })
+})
+
+describe("recursiveReaddir", () => {
+    it("read current dir recursively", async () => {
+        const result = fsp.recursiveReaddir("./")
+        let v = false
+        for await (const file of result) {
+            v = v || file.includes("node_modules") && file.includes("typescript")
+        }
+        assert.isTrue(v)
+    })
+})
+
 describe("exists", () => {
     it("index.ts", async () => {
         const result = await fsp.exists("index.ts")
